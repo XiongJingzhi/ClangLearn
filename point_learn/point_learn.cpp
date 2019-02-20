ddbe9809199f7fad5ebaa2830ea47de4084046c0
XiongJingzhi <davidxiong1994@gmail.com>
2019-02-20 22:39:51 +0800

    sizeof and memory address

--- point_learn/point_learn.cpp
+++ point_learn/point_learn.cpp
@@ -3,7 +3,21 @@
 int main()
 {
     int n = 111;
-    int* p = &n;
-    printf("output\n%p", &p);
+    printf("n adress:%x\n", &n);
+    printf("n value:%d\n", n);
+
+    int* ptr_n = &n;
+    printf("ptr_n address :%x\n", &ptr_n);
+    printf("ptr_n byte%d\n", sizeof(ptr_n));
+    printf("ptr_n value %d\n", ptr_n);
+
+    char ch = 'X';
+    printf("ch address: %d\n", &ch);
+    printf("ch value: %d\n", ch);
+
+    char* ptr_ch = &ch;
+    printf("pt adress%d\n", &ptr_ch);
+    printf("ptr_ch how many byte%d\n", sizeof(ptr_ch));
+    printf("ptr_ch value%d\n", ptr_ch);
     return 0;
 }
